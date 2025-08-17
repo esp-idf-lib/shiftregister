6c7a3faad2215953664d9f2f7ccd98692a742b20
qcrg <david-isupov@bk.ru>
2025-08-17 11:30:01 +0000

    Add active level settings for the SRCRL and OE pin

--- shiftregister_gpio.c
+++ shiftregister_gpio.c
@@ -10,31 +10,40 @@
 #define PULSE_REG_CLOCK_DELAY_US (1)
 #define TAG "shiftregister_gpio"
 
-static esp_err_t gpio_pulse_clock(const gpio_num_t clock)
+static esp_err_t gpio_pulse(const gpio_num_t pin, unsigned int delay_us)
 {
     esp_err_t ret = ESP_FAIL;
 
-    ESP_GOTO_ON_ERROR(gpio_set_level(clock, 1), fail, TAG, "gpio_set_level %s", esp_err_to_name(ret));
-    esp_rom_delay_us(PULSE_CLOCK_DELAY_US);
-    ESP_GOTO_ON_ERROR(gpio_set_level(clock, 0), fail, TAG, "gpio_set_level: %s", esp_err_to_name(ret));
-    ret = ESP_OK;
-fail:
-    return ret;
-
-}
-
-static esp_err_t gpio_pulse_reg_clock(const gpio_num_t reg_clock)
-{
-    esp_err_t ret = ESP_FAIL;
-
-    ESP_GOTO_ON_ERROR(gpio_set_level(reg_clock, 1), fail, TAG, "gpio_set_level %s", esp_err_to_name(ret));
-    esp_rom_delay_us(PULSE_REG_CLOCK_DELAY_US);
-    ESP_GOTO_ON_ERROR(gpio_set_level(reg_clock, 0), fail, TAG, "gpio_set_level %s", esp_err_to_name(ret));
+    ESP_GOTO_ON_ERROR(gpio_set_level(pin, 1), fail, TAG, "gpio_set_level %s", esp_err_to_name(ret));
+    esp_rom_delay_us(delay_us);
+    ESP_GOTO_ON_ERROR(gpio_set_level(pin, 0), fail, TAG, "gpio_set_level: %s", esp_err_to_name(ret));
     ret = ESP_OK;
 fail:
     if (ret != ESP_OK) {
         ESP_LOGE(TAG, "%s", esp_err_to_name(ret));
     }
+    return ret;
+}
+
+static esp_err_t gpio_write(const gpio_num_t pin, const uint32_t bit)
+{
+    esp_err_t ret = ESP_FAIL;
+
+    ESP_GOTO_ON_ERROR(gpio_set_level(pin, bit), fail, TAG, "gpio_set_level: %s", esp_err_to_name(ret));
+    ret = ESP_OK;
+fail:
+    return ret;
+}
+
+static esp_err_t gpio_read(const gpio_num_t pin, uint8_t *data, const uint32_t index)
+{
+    esp_err_t ret = ESP_FAIL;
+    ESP_GOTO_ON_FALSE(data != NULL, ESP_ERR_INVALID_ARG, fail, TAG, "data is NULL");
+
+    /* MSB first */
+    data[index] = ((data[index] << 1) & gpio_get_level(pin));
+    ret = ESP_OK;
+fail:
     return ret;
 }
 
@@ -62,13 +71,21 @@ static esp_err_t shiftregister_gpio_init_other(const shiftregister_config_t *con
 
     io_config.pin_bit_mask = ((1ULL << config->clk_io_num) | (1ULL << config->rclk_io_num));
     io_config.mode = config->mode;
-    if (config->srclr_io_num) {
+    if (config->srclr_io_num != GPIO_NUM_NC) {
         io_config.pin_bit_mask |= (1ULL << config->srclr_io_num);
     }
-    if (config->oe_io_num) {
+    if (config->oe_io_num != GPIO_NUM_NC) {
         io_config.pin_bit_mask |= (1ULL << config->oe_io_num);
     }
     ESP_GOTO_ON_ERROR(gpio_config(&io_config), fail, TAG, "gpio_config %s", esp_err_to_name(ret));
+
+    if (config->srclr_io_num != GPIO_NUM_NC) {
+        ESP_GOTO_ON_ERROR(gpio_write(config->srclr_io_num, !config->srclr_active_level), fail, TAG, "gpio_write %s", esp_err_to_name(ret));
+    }
+    if (config->oe_io_num != GPIO_NUM_NC) {
+        ESP_GOTO_ON_ERROR(gpio_write(config->oe_io_num, config->srclr_active_level), fail, TAG, "gpio_write %s", esp_err_to_name(ret));
+    }
+
     ret = ESP_OK;
 fail:
     return ret;
@@ -80,28 +97,6 @@ esp_err_t shiftregister_gpio_init(const shiftregister_config_t *config)
 
     ESP_GOTO_ON_ERROR(shiftregister_gpio_init_other(config), fail, TAG, "shiftregister_gpio_init_other: %s", esp_err_to_name(ret));
     ESP_GOTO_ON_ERROR(shiftregister_gpio_init_data(config), fail, TAG, "shiftregister_gpio_init_data: %s", esp_err_to_name(ret));
-    ret = ESP_OK;
-fail:
-    return ret;
-}
-
-static esp_err_t gpio_write(const gpio_num_t pin, const uint32_t bit)
-{
-    esp_err_t ret = ESP_FAIL;
-
-    ESP_GOTO_ON_ERROR(gpio_set_level(pin, bit), fail, TAG, "gpio_set_level: %s", esp_err_to_name(ret));
-    ret = ESP_OK;
-fail:
-    return ret;
-}
-
-static esp_err_t gpio_read(const gpio_num_t pin, uint8_t *data, const uint32_t index)
-{
-    esp_err_t ret = ESP_FAIL;
-    ESP_GOTO_ON_FALSE(data != NULL, ESP_ERR_INVALID_ARG, fail, TAG, "data is NULL");
-
-    /* MSB first */
-    data[index] = ((data[index] << 1) & gpio_get_level(pin));
     ret = ESP_OK;
 fail:
     return ret;
@@ -122,10 +117,10 @@ esp_err_t shiftregister_gpio_transfer(const shiftregister_config_t *config, uint
             } else {
                 ESP_GOTO_ON_ERROR(gpio_read(config->data_io_num, data, index), fail, TAG, "gpio_write: %s", esp_err_to_name(ret));
             }
-            ESP_GOTO_ON_ERROR(gpio_pulse_clock(config->clk_io_num), fail, TAG, "gpio_pulse_clock: %s", esp_err_to_name(ret));
+            ESP_GOTO_ON_ERROR(gpio_pulse(config->clk_io_num, PULSE_CLOCK_DELAY_US), fail, TAG, "gpio_pulse_clock: %s", esp_err_to_name(ret));
             ESP_GOTO_ON_ERROR(gpio_write(config->data_io_num, 0), fail, TAG, "gpio_write: %s", esp_err_to_name(ret));
         }
-        ESP_GOTO_ON_ERROR(gpio_pulse_reg_clock(config->rclk_io_num), fail, TAG, "gpio_pulse_reg_clock: %s", esp_err_to_name(ret));
+        ESP_GOTO_ON_ERROR(gpio_pulse(config->rclk_io_num, PULSE_REG_CLOCK_DELAY_US), fail, TAG, "gpio_pulse_reg_clock: %s", esp_err_to_name(ret));
     }
     ret = ESP_OK;
 fail:
